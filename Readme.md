3d0178bddaa7bfb90462efaedff650a14be34d58
Anuj Jindal <anujjindal121@yahoo.com>
2019-02-04 20:36:31 +0530

    added morgan and nodemon

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Node Tools/lib Used in this project
+
+- express (WebServer For Node)
+- Routes (Build Api Routes)
+- [Nodemon](https://github.com/remy/nodemon)
+  - Monitor for any changes in your node.js application and automatically restart the server - perfect for development http://nodemon.io/
+- Morgan(logging package for node)
--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-// app.use((req, res, next) => {
-//     res.status(200).json({
-//         "message": "OK",
-//         "code": 200,
-//     })
-// })
-app.use('/products',productRoutes)
-app.use('/orders',orderRoutes)
+app.use(morgan('dev'));
+
+app.use('/products',productRoutes);
+app.use('/orders',orderRoutes);
+
+//no rute here because want to recive ever error case
+app.use((req,res,next)=>{
+const error = new Error('Not Found');
+error.status=404;
+next(error);
+}
+);
+
+//handle error if everything gets fails
+app.use((error,req,res,next)=>{
+res.status(error.status||500);
+res.json({
+ error :{
+     message: error.message
+ }
+});
+});
+
 module.exports = app;
