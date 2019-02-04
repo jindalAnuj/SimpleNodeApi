175d0ff8b1195b6647123222a0e0e8311800d2c2
Anuj Jindal <anujjindal121@yahoo.com>
2019-02-04 18:14:59 +0530

    basic get req 200 received

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.use((req, res, next) => {
+    res.status(200).json({
+        "message": "OK",
+        "code": 200,
+    })
+})
+module.exports = app;
